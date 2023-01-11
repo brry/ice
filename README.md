dd425b47f40783513750208b6885aa73df6aed14
brry <berryboessenkool@hotmail.com>
2023-01-11 08:14:27 +0100

    update

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 Whenever it gets cold enough during the winter, I love to play ice
 hockey on the lakes in Potsdam, see
-[bit.ly/eis\_mt](https://bit.ly/eis_mt).  
+[bit.ly/eis_mt](https://bit.ly/eis_mt).  
 This hobby started 2012: Till Francke gave a course on numerical
 modelling in which we predicted ice thickness. I told about my ice
 skating experience (as model verification) and he invited me out to
@@ -30,19 +30,23 @@ days all had a maximum temperature below zero.
 
 ![](README_files/figure-gfm/icemodel-1.png)<!-- -->
 
-This is good enough for me\! I see some potentially missed chances in
+This is good enough for me! I see some potentially missed chances in
 earlier years and the results of some aggressive advertising through our
-mailing list in the spirit of **“the ice shall hold\!”** later on (we
+mailing list in the spirit of **“the ice shall hold!”** later on (we
 never broke through the ice).  
 So next time after two or three days of frost, I will be checking 10
 different weather prognosis services - and yet probably be even more
 excited than I was in previous years.
 
+### vis
+
+Here are the playable days (<span style="color:blue">\*</span>) along
+with temperature: ![](README_files/figure-gfm/icevis-1.png)<!-- -->
+
 ### play
 
-You live near Potsdam and want to join? You can\! Very much so\!  
-Subscribe to the mailing list at
-[bit.ly/eis\_mt](https://bit.ly/eis_mt).
+You live near Potsdam and want to join? You can! Very much so!  
+Subscribe to the mailing list at [bit.ly/eis_mt](https://bit.ly/eis_mt).
 
 ### code
 
@@ -67,7 +71,7 @@ The data in the first years is not as complete as in later years
 (especially 2012 when I was late to the party). With the increasing
 number of people in the mailing list, lag time decreased ^^. Also, in
 2019 we discovered a pond that freezes much earlier than the lakes we
-played on previously (again, see [bit.ly/eis\_mt](https://bit.ly/eis_mt)
+played on previously (again, see [bit.ly/eis_mt](https://bit.ly/eis_mt)
 for the locations).  
 I did not play on all the days myself, but also recorded when other
 people from our mailing list were on.
--- icePlot.R
+++ icePlot.R
@@ -9,9 +9,12 @@ icePlot <- function(
 days,             # (sequential) calender dates
 ice,              # TRUE/FALSE vector - actual playing possibility
 model=NULL,       # TRUE/FALSE vector - model prediction (optional)
-winter_only=TRUE, # cut to the playing season January-March?
+temp=NULL,        # Numerical: temperature lines to be displayed
+winter_only=FALSE,# cut to the playing season January-March?
 plot=TRUE,        # plot figure? 
 label_gof=TRUE,   # Label CCR?
+staroffset=0.02,  # Offset for stars
+addyears=1,       # Number of years to add at the bottom for the legend
 main="Ice hockey Potsdam", # Graph title
 ...               # Aguments passed to points (beware some are hard coded, like x,y,pch,col,cex)
 )
@@ -29,15 +32,15 @@ if(winter_only)
   }
 #
 # helper function to add points at day of year (doy):
-add_doys <- function(dates, offset=0, ...)
+add_doys <- function(dates, offset=0, values=NULL, ...)
   {
-  doy <- tapply(dates, format(dates, "%Y"), function(d) as.Date(format(d, "2016-%m-%d")))
-  for(y in names(doy)) 
-    {
-    doy_i <- doy[[y]]
-    ycoord <- rep(as.numeric(y), length(doy_i))
-    points(doy_i, ycoord-offset, ...)
-    }
+  # browser()
+  dec <- format(dates,"%m") > "08"
+  dx <- format(dates, paste0(ifelse(dec,2015,2016),"-%m-%d"))
+  dy <- as.numeric(format(dates, "%Y"))
+  dy[dec] <- dy[dec]+1
+  if(is.null(values)) return(points(as.Date(dx), dy-offset, ...) )
+  segments(x0=as.Date(dx), y0=dy, y1=dy-values/20, col=ifelse(values>0,"grey","red"))
   }
 #
 # compute model goodness of fit:
@@ -54,34 +57,33 @@ if(!is.null(model))
 # plot:
 if(plot)
   {
-  op <- par(mar=c(3,4,2,0.5))
+  op <- par(mar=c(2,0.5,2,3.5))
   on.exit(par(op), add=TRUE)
   # empty graph, axes etc:
   minyear <- as.numeric(format(min(days),"%Y"))
-  maxyear <- as.numeric(format(max(days),"%Y"))
-  plot(as.Date("2016-01-01")+c(0,90), c(1,1), type="n", ylim=c(maxyear, minyear), 
-       xaxt="n", ylab="Year", xlab="", yaxt="n", main=main)
+  maxyear <- as.numeric(format(max(days),"%Y")) + addyears
+  plot(as.Date("2016-01-01")+c(-30,90), c(1,1), type="n", ylim=c(maxyear, minyear), 
+       xaxt="n", ylab="", xlab="", yaxt="n", main=main)
   berryFunctions::monthAxis(mlabels=month.abb, yformat=" ", quiet=TRUE, 
                             ytcl=par("tcl"), mcex=1, mline=-0.5)
-  title(xlab="Date", line=1.7)
   abline(h=minyear:maxyear, col=8)
-  axis(2, minyear:maxyear, las=1, mgp=c(3,0.7,0))
-  # add points for actual playing days:
-  add_doys(days[ice], offset=0.02, col="blue", pch="*", cex=1.2, ...)  # -0.02 offset to have the stars on the line
+  axis(4, minyear:maxyear, las=1, mgp=c(3,0.7,0))
   #
-  if(!is.null(model))
   # add points for model prediction
+  if(!is.null(model))
   add_doys(days[model], col="grey", pch="|", cex=1.2, ...)
   if(label_gof & !is.na(ccr)) mtext(paste0("Correct: ", 
                                     berryFunctions::round0(ccr*100,1), "%"), 
                                     line=1, adj=1, outer=FALSE, col=8)
+  # add temperature graphs:
+  if(!is.null(temp))
+  add_doys(days, values=temp)
+  # add points for actual playing days:
+  add_doys(days[ice], offset=staroffset, col="blue", pch="*", cex=1.2, ...)  # -0.02 offset to have the stars on the line
   } # plot end
 # output:
 return(c(ccr=ccr))
 } # icePlot function end
-
-
-
 
 if(!requireNamespace("berryFunctions", quietly=TRUE)) 
   stop("please first run:\ninstall.packages('berryFunctions')" )
